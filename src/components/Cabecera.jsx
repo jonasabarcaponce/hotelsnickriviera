324635d797a3e521bd1ae9793315b110300ebad7
Jonas Abarca <jonasabarca@MacBook-Air-de-Jonas.local>
2025-02-13 13:47:33 -0500

    Cabecera

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -10,149 +10,73 @@ import image1 from '../assets/bg-2.jpg';
 import image2 from '../assets/bg.png';
 
 const imagesWithTitles = [
-    { src: image1, title: 'Disfruta tu vacaciones como nunca', subtitle: 'Junto a Bob Esponja, Las Tortugas Ninja y muchos otros personajes de Nick en una escapada inolvidable.' },
-    { src: image2, title: '¡Ahorra hasta un 30% de descuento!', subtitle: 'Reserva ahora con nosotros para obtener todos los beneficios' },
+    { src: image1, title: 'Disfruta tu vacaciones como nunca', subtitle: <>Junto a Bob Esponja, Las Tortugas Ninja y  <br className="hidden sm:block" />otros personajes de Nick en una escapada inolvidable.</> },
+    { src: image2, title: '¡Ahorra hasta un 30% de descuento!', subtitle: <>Obtén beneficios únicos reservando ahora.<br className="hidden sm:block" />Promoción exclusiva de este sitio web.</> },
 ];
-
-// Reusable Sidenav Component
-const NavegacionLateral = ({ isOpen, toggleSideNav }) => {
-    return (
-        <>
-            {/* Overlay */}
-            <div
-                className={`fixed inset-0 bg-black bg-opacity-50 z-10 transition-opacity duration-300 ${
-                    isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
-                }`}
-                onClick={toggleSideNav}
-            ></div>
-            {/* Sidenav */}
-            <div
-                className={`fixed top-0 left-0 w-64 h-full bg-nickelodeonGreen shadow-md z-20 transform transition-transform duration-300 ${
-                    isOpen ? 'translate-x-0' : '-translate-x-full'
-                }`}
-            >
-                <button
-                    onClick={toggleSideNav}
-                    className="absolute top-4 right-4 text-white focus:outline-none"
-                >
-                    <i className="fas fa-times"></i>
-                </button>
-                <div className="py-10 px-5">
-                    <ul className="space-y-5">
-                        <li>
-                            <a href="#cotizar" onClick={toggleSideNav} className="text-white/80 hover:text-white">
-                                Alojamientos
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#cotizar" onClick={toggleSideNav} className="text-white/80 hover:text-white">
-                                Experiencias
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#cotizar" onClick={toggleSideNav} className="text-white/80 hover:text-white">
-                                Aqua Nick
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#cotizar" onClick={toggleSideNav} className="text-white/80 hover:text-white">
-                                Alimentos y Bebidas
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#cotizar" onClick={toggleSideNav} className="text-white/80 hover:text-white">
-                                Bodas
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#cotizar" onClick={toggleSideNav} className="text-white/80 hover:text-white">
-                                Grupos y Celebraciones
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#cotizar" onClick={toggleSideNav} className="text-white/80 hover:text-white">
-                                Ofertas
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#cotizar" onClick={toggleSideNav} className="text-white/80 hover:text-white">
-                                Galería
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#cotizar" onClick={toggleSideNav} className="text-white/80 hover:text-white">
-                                Check-In Digital
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#cotizar" onClick={toggleSideNav} className="text-white/80 hover:text-white">
-                                Tienda en Línea
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#cotizar" onClick={toggleSideNav} className="text-white/80 hover:text-white">
-                                Contáctanos
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </>
-    );
-};
 
 // Main Header Component
 const Cabecera = () => {
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggleSideNav = () => {
+    const menuToggle = () => {
         setIsOpen(!isOpen);
     };
 
     return (
         <>
-            <NavegacionLateral isOpen={isOpen} toggleSideNav={toggleSideNav} />
-            {/* Main Header Section */}
-            <Swiper
-                modules={[Autoplay]}
-                autoplay={{
-                    delay: 3000, // Delay between transitions in milliseconds
-                    disableOnInteraction: true, // Keeps autoplay running even after interaction
-                }}
-                loop={true}
-            >
-                {imagesWithTitles.map((item, index) => (
-                    <SwiperSlide
-                        key={index}
-                        className="relative h-screen bg-no-repeat bg-cover bg-right sm-bg-center"
-                        style={{ backgroundImage: `url(${item.src})` }}
-                    >
-                        <div className="absolute inset-0 bg-black opacity-50"></div>
-                        <div className="relative max-w-[986px] mx-auto py-10 px-5">
-                            <nav className="flex justify-between items-center">
-                                <div>
-                                    <img src={logoImage} className="w-6/12 md:w-8/12" alt="Logo" />
+        <header className="bg-white text-black relative px-5">
+            <div className="mx-auto py-8 flex justify-between px-5 items-center max-w-[1200px]">
+                <div>
+                    <a href="/" title="C.G. Desarrollos - Conexión Global Desarrollos">
+                        <img src={logoImage} className="w-6/12 md:w-8/12" alt="Logo" />
+                    </a>
+                </div>
+                <div className="md:hidden">
+                    <button id="menu-toggle" className="bg-black text-white px-4 py-2 rounded-full" onClick={menuToggle}>
+                        <span id="menu-toggle-icon"><i className="fas fa-bars"></i></span>
+                    </button>
+                </div>
+                <nav id="menu" className={`md:flex flex-col md:flex-row bg-white md:bg-transparent w-full md:w-auto space-y-5 md:space-y-0 md:space-x-5 font-bold items-start md:items-center p-5 md:p-0 shadow-md md:shadow-none absolute md:relative top-full left-0 md:top-0 md:left-auto z-50 ${isOpen ? 'block' : 'hidden'}`}>
+                    <a href="#" className="block">Inicio</a>
+                    <a href="#nosotros" className="block">Habitaciones</a>
+                    <a href="#desarrollos" className="block">Información</a>
+                    <a href="#recorridos" className="block">Reservar</a>
+                    <a href="#" className="block bg-black text-white px-8 py-2 rounded transition-opacity hover:opacity-80">Llamar</a>
+                 </nav>
+            </div>
+        </header>
+
+        <Swiper
+            modules={[Autoplay]}
+            autoplay={{
+                delay: 3000, // Delay between transitions in milliseconds
+                disableOnInteraction: true, // Keeps autoplay running even after interaction
+            }}
+            loop={true}
+        >
+            {imagesWithTitles.map((item, index) => (    
+                <SwiperSlide key={index}>
+                    <div className='relative py-10 md:py-36 px-5'>
+                        <div className="mx-auto px-5 items-center max-w-[1200px]">
+                            <img src={item.src} alt="right" className='w-full md:w-1/3 md:absolute bottom-0 right-0 rounded-2xl md:rounded-br-none mb-10 md:mb-0'/>
+                                <div className="container relative mx-auto">
+                                    <h1 className="text-2xl md:text-3xl font-bold">
+                                        {item.title}
+                                    </h1>
+                                    <p className="text-lg mt-5">
+                                        {item.subtitle}
+                                    </p>
+                                    <div className="mt-8 flex space-x-4">
+                                        <a href="#" className="bg-black text-white text-xs md:text-base px-4 md:px-8 py-2 rounded transition-opacity hover:opacity-80">Ver habitaciones</a>
+                                        <a href="#" className="bg-gray-300 text-black text-xs md:text-base px-4 md:px-8 py-2 rounded transition-opacity hover:opacity-80">Reservar</a>
+                                    </div>
                                 </div>
-                                <div className="flex flex-row space-x-2">
-                                    <a
-                                        href="#"
-                                        className="bg-black text-xl text-white flex items-center justify-center rounded-full min-w-12 w-12 h-12"
-                                        onClick={toggleSideNav}
-                                    >
-                                        <i className="fas fa-bars"></i>
-                                    </a>
-                                </div>
-                            </nav>
                         </div>
-                        <div className="relative flex items-center max-w-[986px] mx-auto px-5 h-4/6 font-bold text-white">
-                            <div>
-                                <h1 className="mb-3 big-responsive-text">{item.title}</h1>
-                                <p className="medium-responsive-text">{item.subtitle}</p>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+                    </div>
+                    
+                </SwiperSlide>
+            ))}
+        </Swiper>
         </>
     );
 };
